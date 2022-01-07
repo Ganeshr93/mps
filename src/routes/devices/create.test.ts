435847cd50cafae40d8e5f094b05b460e3bc9002
Barney Cheng <barney.cheng@intel.com>
2022-01-07 09:07:50 -0800

    test: adds unit tests for devices
    closes AB#5499

---
+++ src/routes/devices/create.test.ts
@@ -0,0 +1,10 @@
+afterEach(() => {
+  jest.clearAllMocks()
+  jest.resetModules()
+})
+
+describe('Create device', () => {
+  it('should return 200 if device alreeady exists in db', () => {
+
+  })
+})
